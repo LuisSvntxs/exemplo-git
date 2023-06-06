2ce80e92ee580a13911b948263c4100ac4b13845
LuisSvntxs <luisscopo@gmail.com>
2023-06-06 20:12:38 -0300

    Entrada da idade do usuário

--- LivrariaApplication.java
+++ LivrariaApplication.java
@@ -9,7 +9,20 @@ public class LivrariaApplication {
         Scanner scan = new Scanner(System.in);
         System.out.println("Digite seu nome");
         String nome = scan.nextLine();
-        scan.close();
+
         System.out.println("Ola, " + nome + "!");
+
+        System.out.println("Digite sua idade");
+        int idade = scan.nextInt();
+
+        if (idade < 18) {
+
+            System.out.println(nome + ", sua idade é: " + idade + ". Você é menor de idade!");
+        } else {
+            System.out.println(nome + ", sua idade é: " + idade + ". Você é maior de idade!");
+
+        }
+
+        scan.close();
     }
 }
